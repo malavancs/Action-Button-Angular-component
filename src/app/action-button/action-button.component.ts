c468232b58f90632bea609b7cdfb67780d45a3b0
malavancs <mlvnhari@gmail.com>
2019-12-04 17:21:46 +0530

    Added component @input,output

--- src/app/action-button/action-button.component.ts
+++ src/app/action-button/action-button.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Input,Output ,EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-action-button',
   templateUrl: './action-button.component.html',
   styleUrls: ['./action-button.component.css']
 })
-export class ActionButtonComponent implements OnInit {
 
+export class ActionButtonComponent implements OnInit {
+  @Input() buttons:{label:string,color:string,icon:string,value,background:string}[];
+  @Output() change: EventEmitter<any> = new EventEmitter();
+  selectedIndex: number;
   constructor() { }
 
   ngOnInit() {
+    this.selectedIndex = 0;
   }
+changeButton(){
+  if(this.buttons.length === (this.selectedIndex+1)) {
+    this.selectedIndex = 0;
+  } else {
+    this.selectedIndex++;
+  }
+  this.change.emit(this.buttons[this.selectedIndex]);
 
+}
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular';
+  options: any;
+  ngOnInit() {
+    this.options = [{
+      background: "#aeaedf",
+      color: "white",
+      icon: "fa-info",
+      label: "Select ",
+      value: 0,
+    }, {
+      background: "green",
+      color: "white",
+      icon: "fa fa-check-square",
+      label: "Approve",
+      value: 1,
+    }, {
+      background: "#aeaedf",
+      color: "white",
+      icon: "fa fa-square",
+      label: "Ignore",
+      value: 1,
+    }];
+  }
+  onChanged(i) {
+    console.log("malavan", i);
+  }
 }
